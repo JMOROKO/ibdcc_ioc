c66446bc8663278b604f18eca07d0b919b644338
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-02-18 13:27:06 +0000

    create dao and metier

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+A
+src/dao/IDao.java,4\a\4af3e7f225e645b4349f32affb9d00ed6eac68c1
+D
+src/dao/DaoImpl.java,d\5\d584bfd50d49dbe260c5e983392d46815ed07e00
+G
+src/metier/IMetier.java,e\9\e96afc1dc794f0bd917fa08e8a77be63c29f5cd2
+J
+src/metier/MetierImpl.java,3\5\35b6a1281d4616769c7e12c023a20bf9f3390923
---
+++ src/metier/MetierImpl.java
@@ -0,0 +1,22 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao;
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcule() {
+        double tmp = dao.getData();
+        double res = tmp * 540/Math.cos(tmp*Math.PI);
+        return res;
+    }
+}
